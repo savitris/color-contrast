b8d4b45c91bd8a301532e63210cc803ad8196bc3
savitris <11764984+savitris@users.noreply.github.com>
2023-11-12 21:38:11 +0100

    🎉  initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # color-contrast
+
 node script to check color contrast in themes and offer AA meeting shades
+
+Make sure to install the 'color' package using `npm install color` before running the script.
+
+This script defines two functions: `getContrastRatio` to calculate the contrast ratio between two colors, and `findAccessibleTextColor` to find a suitable text color that meets the WCAG contrast ratio requirements. Adjust the `backgroundColor` and `textColors` variables with your desired colors.
---
+++ index.js
@@ -0,0 +1,41 @@
+const Color = require("color");
+
+function getContrastRatio(color1, color2) {
+  const luminance1 = Color(color1).luminosity();
+  const luminance2 = Color(color2).luminosity();
+
+  const brighter = Math.max(luminance1, luminance2);
+  const darker = Math.min(luminance1, luminance2);
+
+  return (brighter + 0.05) / (darker + 0.05);
+}
+
+function findAccessibleTextColor(background, textColorOptions) {
+  const MIN_CONTRAST_RATIO = 4.5; // WCAG AA standard
+
+  for (const textColor of textColorOptions) {
+    const contrastRatio = getContrastRatio(textColor, background);
+
+    if (contrastRatio >= MIN_CONTRAST_RATIO) {
+      return textColor;
+    }
+  }
+
+  // If no suitable text color is found, you might want to handle this case accordingly
+  return null;
+}
+
+// Example usage
+const backgroundColor = "#000"; // Replace with your background color
+const textColors = ["#fff", "#333", "#aabbcc"]; // Replace with your text color options
+
+const accessibleTextColor = findAccessibleTextColor(
+  backgroundColor,
+  textColors
+);
+
+if (accessibleTextColor) {
+  console.log(`Accessible text color: ${accessibleTextColor}`);
+} else {
+  console.log("No accessible text color found.");
+}
